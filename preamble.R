765e832e7121fcaa1bf7d2e242677cb7097f8535
Owen Jones <jones@biology.sdu.dk>
2020-09-06 22:31:46 +0200

    Added life tables practical

--- preamble.R
+++ preamble.R
@@ -1,5 +1,11 @@
+#packages for the main content
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(magrittr)
 library(patchwork)
+
+#Packages for the web pages
+library(xfun)
+library(htmltools)
+library(mime)
