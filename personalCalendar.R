a1989aa4c9e02f44f6250bc0764c6c9dc8dba18c
Owen Jones <jones@biology.sdu.dk>
2020-09-10 14:34:41 +0200

    Create personalised ical subscriptions

--- personalCalendar.R
+++ personalCalendar.R
@@ -1,4 +1,4 @@
-#Create personal calendar for Owen and Thomas
+#Create personal calendar for Owen and Thomas-----
 
 courseCal <- ic_read("bb512.ics") %>% 
   arrange(DTSTART) %>% 
@@ -18,40 +18,40 @@ icalSchedule <- left_join(courseCal,schedule) %>%
 
 str(icalSchedule)
 
-if(dir.exists("personalCalendar_Owen")){system("rm -r personalCalendar_Owen")}
-dir.create("personalCalendar_Owen")
-icalScheduleTemp <- icalSchedule %>% filter(Instructor == "OJ")
-for(i in 1:nrow(icalScheduleTemp)){
-icalName <- paste0("BB512_",icalScheduleTemp$DTSTART2[i])
-ic_write(file = paste0("personalCalendar_Owen/",icalName,".ics") ,ic = ic_event(start = icalScheduleTemp$DTSTART2[i], 
-                    end = icalScheduleTemp$DTEND2[i] , 
-                    summary = paste0("BB512 ", icalScheduleTemp$Topic[i],", ", icalScheduleTemp$Type[i], " in ", icalScheduleTemp$Room[i])))
+instructorList <- na.omit(unique(icalSchedule$Instructor))
+
+
+for(inst in 1:length(instructorList)){
+  if(dir.exists(paste0("personalCalendar_",instructorList[inst]))){
+    system(paste0("rm -r personalCalendar_",instructorList[inst]))}
+  dir.create(paste0("personalCalendar_",instructorList[inst]))
+  
+  icalScheduleTemp <- icalSchedule %>% filter(Instructor == instructorList[inst])
+  
+  for(i in 1:nrow(icalScheduleTemp)){
+    icalName <- paste0("BB512_",icalScheduleTemp$DTSTART2[i])
+    ic_write(file = paste0("personalCalendar_",instructorList[inst],"/",icalName,".ics"),
+             ic = ic_event(start = icalScheduleTemp$DTSTART2[i],
+                           end = icalScheduleTemp$DTEND2[i],
+                           summary = paste0("BB512 ", icalScheduleTemp$Topic[i],", ",
+                                            icalScheduleTemp$Type[i], " in ", icalScheduleTemp$Room[i])))
+  }
+  
+  #Combine event files by appending the text
+  files <- list.files(paste0("personalCalendar_",instructorList[inst]),full.names  = TRUE)
+  combinedFiles <- NULL
+  for(i in 1:length(files)){
+    temp <- readLines(con = files[i])  
+    combinedFiles <- append(combinedFiles,temp)
+  }
+  
+  #Remove the Begin/End Calendar lines.
+  combinedFiles[grep(pattern = "*:VCALENDAR",combinedFiles)]<-""
+  
+  #Add begin/end calendar lines to the text.
+  combinedFiles[1] <- "BEGIN:VCALENDAR"
+  combinedFiles[length(combinedFiles)] <- "END:VCALENDAR"
+  
+  writeLines(combinedFiles,con = paste0("BB512_",instructorList[inst],".ics"))
+  
 }
-
-if(dir.exists("personalCalendar_Thomas")){system("rm -r personalCalendar_Thomas")}
-dir.create("personalCalendar_Thomas")
-icalScheduleTemp <- icalSchedule %>% filter(Instructor == "TBB")
-for(i in 1:nrow(icalScheduleTemp)){
-  icalName <- paste0("BB512_",icalScheduleTemp$DTSTART2[i])
-  ic_write(file = paste0("personalCalendar_Thomas/",icalName,".ics") ,ic = ic_event(start = icalScheduleTemp$DTSTART2[i], 
-                                                                                  end = icalScheduleTemp$DTEND2[i] , 
-                                                                                  summary = paste0("BB512 ", icalScheduleTemp$Topic[i],", ", icalScheduleTemp$Type[i], " in ", icalScheduleTemp$Room[i])))
-}
-
-#Combine files by appending the text
-files <- list.files("personalCalendar_Owen/",full.names  = TRUE)
-combinedFiles <- NULL
-for(i in 1:length(files)){
-  temp <- readLines(con = files[i])  
-  combinedFiles <- append(combinedFiles,temp)
-}
-
-#Remove the Begin/End Calendar lines.
-combinedFiles[grep(pattern = "*:VCALENDAR",combinedFiles)]<-""
-
-#Add begin/end calendar lines to the text.
-combinedFiles[1] <- "BEGIN:VCALENDAR"
-combinedFiles[length(combinedFiles)] <- "END:VCALENDAR"
-
-writeLines(combinedFiles,con = "BB512_Owen.ics")
-
